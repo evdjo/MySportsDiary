10126976d2ef92c6e80e9627a169087d354e0a80
Evdzhan Mustafa <enm3@lp01enm3.stunet.aber.ac.uk>
2016-04-12 15:59:30 +0100

    Fixed command launch args.

--- MySportsDiary/MainDataManager.swift
+++ MySportsDiary/MainDataManager.swift
@@ -10,71 +10,72 @@ import Foundation
 import UIKit
 
 class MainDataManager: DataManager {
-	internal init() { };
-	func getAge() -> Int? { return UserProperties.getAge(); }
-	func getGender() -> Gender? { return UserProperties.getGender(); }
+    internal init() { };
+    func getAge() -> Int? { return UserProperties.getAge(); }
+    func getGender() -> Gender? { return UserProperties.getGender(); }
 
-	func setAge(age: Int) { UserProperties.setAge(age); }
-	func setGender(gender: Gender) { UserProperties.setGender(gender); }
+    func setAge(age: Int) { UserProperties.setAge(age); }
+    func setGender(gender: Gender) { UserProperties.setGender(gender); }
 
-	func setAnswer(questionID: Int, answer: Int) { QuestionnaireAnswers.setAnswer(questionID, answer: answer); }
-	func getAnswer(questionID: Int) -> Int? { return QuestionnaireAnswers.getAnswer(questionID); }
+    func setAnswer(questionID: Int, answer: Int) { QuestionnaireAnswers.setAnswer(questionID, answer: answer); }
+    func getAnswer(questionID: Int) -> Int? { return QuestionnaireAnswers.getAnswer(questionID); }
 
-	func setAppState(appState: ApplicationState) { AppProperties.setAppState(appState); }
-	func getAppState() -> ApplicationState? { return AppProperties.getAppState(); }
+    func setAppState(appState: ApplicationState) { AppProperties.setAppState(appState); }
+    func getAppState() -> ApplicationState? { return AppProperties.getAppState(); }
 
-	func setTempImages(images: Array<UIImage>) { return TemporaryImages.setTempImages(images); }
-	func getTempImages() -> Array<UIImage>? { return TemporaryImages.getTempImages(); }
-	func saveTempImage(image: UIImage) { TemporaryImages.saveTempImage(image); }
-	func removeTempImage(index: Int) { TemporaryImages.removeTempImage(index); }
-	func getImagesCount() -> Int { return TemporaryImages.getImagesCount(); }
-	func moveTempImages(toDir dir: String) -> NSURL { return TemporaryImages.moveTempImages(toDir: dir) }
+    func setTempImages(images: Array<UIImage>) { return TemporaryImages.setTempImages(images); }
+    func getTempImages() -> Array<UIImage>? { return TemporaryImages.getTempImages(); }
+    func saveTempImage(image: UIImage) { TemporaryImages.saveTempImage(image); }
+    func removeTempImage(index: Int) { TemporaryImages.removeTempImage(index); }
+    func getImagesCount() -> Int { return TemporaryImages.getImagesCount(); }
+    func moveTempImages(toDir dir: String) -> NSURL { return TemporaryImages.moveTempImages(toDir: dir) }
 
-	func getTempVideo() -> NSURL? { return TemporaryVideo.getTempVideo(); }
-	func setTempVideo(videoURL: NSURL?) { TemporaryVideo.setTempVideo(videoURL); }
+    func getTempVideo() -> NSURL? { return TemporaryVideo.getTempVideo(); }
+    func setTempVideo(videoURL: NSURL?) { TemporaryVideo.setTempVideo(videoURL); }
 
-	func getTempAudio() -> (url: NSURL, exists: Bool) { return TemporaryAudio.getTempAudio(); }
-	func setTempAudio(audioURL: NSURL?) { TemporaryAudio.setTempAudio(audioURL); }
+    func getTempAudio() -> (url: NSURL, exists: Bool) { return TemporaryAudio.getTempAudio(); }
+    func setTempAudio(audioURL: NSURL?) { TemporaryAudio.setTempAudio(audioURL); }
 
-	func addNewEntry(entry: Entry) {
-		insertEntry(entry)
-	}
+    func addNewEntry(entry: Entry) {
+        insertEntry(entry)
+    }
 
-	func getEntries() -> [Entry]? {
-		return entries();
-	}
+    func getEntries() -> [Entry]? {
+        return entries();
+    }
 
-	func getEntryForID(entry_id: Int64) -> Entry? {
-		return entryForID(entry_id);
-	}
+    func getEntryForID(entry_id: Int64) -> Entry? {
+        return entryForID(entry_id);
+    }
 
-	func purgeEntriesDB() {
-		deleteFile(file: DB_FILE_URL);
-	}
+    func purgeEntriesDB() {
+        deleteFile(file: DB_FILE_URL);
+    }
 
-	///
-	/// CAUTION!  Does what you think! (Deletes everything)
-	/// Used for testing purposes.
-	///
-	func purgeAllData() {
-		UserProperties.purgeData();
-		AppProperties.purgeData();
-		QuestionnaireAnswers.purgeData();
-		self.purgeTempMedia()
-	}
+    ///
+    /// CAUTION!  Does what you think! (Deletes everything)
+    /// Used for testing purposes.
+    ///
+    func purgeAllData() {
+        UserProperties.purgeData();
+        AppProperties.purgeData();
+        QuestionnaireAnswers.purgeData();
+        self.purgeTempMedia()
+        self.purgeEntriesDB();
+    }
 
-	func purgeUserData() {
-		UserProperties.purgeData();
-	}
-	func purgeAppData() {
-		AppProperties.purgeData();
-	}
-	func purgeQuestionnaireAnswers() {
-		QuestionnaireAnswers.purgeData();
-	}
-	func purgeTempMedia() {
-		TemporaryImages.purgeImages();
-		TemporaryAudio.purgeTempAudio();
-		TemporaryVideo.purgeTempVideo();
-	}
+    func purgeUserData() {
+        UserProperties.purgeData();
+    }
+    func purgeAppData() {
+        AppProperties.purgeData();
+    }
+    func purgeQuestionnaireAnswers() {
+        QuestionnaireAnswers.purgeData();
+    }
+    func purgeTempMedia() {
+        TemporaryImages.purgeImages();
+        TemporaryAudio.purgeTempAudio();
+        TemporaryVideo.purgeTempVideo();
+    }
 }
--- MySportsDiary/NewEntryDescriptionAndMediaVC.swift
+++ MySportsDiary/NewEntryDescriptionAndMediaVC.swift
@@ -12,159 +12,160 @@ import MobileCoreServices
 
 class NewEntryDescriptionAndMediaVC: UIViewController, UIPopoverPresentationControllerDelegate, UITextViewDelegate, MediaCountDelegate {
 
-	private var promptText: String {
-		get {
-			return "Tell us how rugby helped you to demonstrate " +
-				"\(skill.lowercaseString) today."
-		}
-	}
-	var skill: String = "";
+    private var promptText: String {
+        get {
+            return "Tell us how rugby helped you to demonstrate " +
+                "\(skill.lowercaseString) today."
+        }
+    }
+    var skill: String = "";
 
-	@IBOutlet weak var audioCountLabel: UILabel!
-	@IBOutlet weak var videoCountLabel: UILabel!
-	@IBOutlet weak var imagesCountLabel: UILabel!;
-	@IBOutlet weak var descriptionTextArea: UITextView!
-	@IBOutlet weak var tellUsHowLabel: UILabel!
+    @IBOutlet weak var audioCountLabel: UILabel!
+    @IBOutlet weak var videoCountLabel: UILabel!
+    @IBOutlet weak var imagesCountLabel: UILabel!;
+    @IBOutlet weak var descriptionTextArea: UITextView!
+    @IBOutlet weak var tellUsHowLabel: UILabel!
 
-	override func viewDidLoad() {
-		super.viewDidLoad();
-		descriptionTextArea.delegate = self;
-		descriptionTextArea.text = enterText;
-		tellUsHowLabel.text = promptText;
-	}
-	override func viewWillAppear(animated: Bool) {
-		super.viewWillAppear(animated);
-		updateImagesCount();
-		updateVideoCount();
-		updateAudioCount();
-	}
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        descriptionTextArea.delegate = self;
+        descriptionTextArea.text = enterText;
+        tellUsHowLabel.text = promptText;
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+        updateImagesCount();
+        updateVideoCount();
+        updateAudioCount();
+    }
 
-	override func viewWillDisappear(animated: Bool) {
-		super.viewWillDisappear(animated);
-		if (navigationController?.topViewController != self) {
-			self.navigationController?.navigationBarHidden = false;
-		}
-	}
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated);
+        if (navigationController?.topViewController != self) {
+            self.navigationController?.navigationBarHidden = false;
+        }
+    }
 
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-		self.view.alpha = 0.20;
-		let controller = segue.destinationViewController;
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        self.view.alpha = 0.20;
+        let controller = segue.destinationViewController;
 
-		if segue.identifier == "photoSegue" || segue.identifier == "videoSegue" {
-			controller.preferredContentSize = CGSize(width: view.frame.width, height: view.frame.height);
-		} else if segue.identifier == "audioSegue" {
-			controller.preferredContentSize = CGSize(width: view.frame.width, height: 60);
-		}
+        if segue.identifier == "photoSegue" || segue.identifier == "videoSegue" {
+            controller.preferredContentSize = CGSize(width: view.frame.width, height: view.frame.height);
+        } else if segue.identifier == "audioSegue" {
+            controller.preferredContentSize = CGSize(width: view.frame.width, height: 60);
+        }
 
-		if let controller = controller as? MediaContainer {
-			controller.mediaCountDelegate = self;
-		}
+        if let controller = controller as? MediaContainer {
+            controller.mediaCountDelegate = self;
+        }
 
-		controller.popoverPresentationController?.delegate = self;
-		controller.popoverPresentationController?.sourceRect = (sender as! UIButton).bounds;
-		controller.popoverPresentationController?.backgroundColor = blue;
-	}
+        controller.popoverPresentationController?.delegate = self;
+        controller.popoverPresentationController?.sourceRect = (sender as! UIButton).bounds;
+        controller.popoverPresentationController?.backgroundColor = blue;
+    }
 
-	func popoverPresentationControllerDidDismissPopover(_: UIPopoverPresentationController) {
-		self.view.alpha = 1;
-	}
+    func popoverPresentationControllerDidDismissPopover(_: UIPopoverPresentationController) {
+        self.view.alpha = 1;
+    }
 
-	func onCountChange(sender: UIViewController) {
-		if sender is ImagesPopoverVC {
-			updateImagesCount();
-		} else if sender is VideoPopoverVC {
-			updateVideoCount();
-		} else if sender is AudioRecorderVC {
-			updateAudioCount();
-		}
-	}
+    func onCountChange(sender: UIViewController) {
+        if sender is ImagesPopoverVC {
+            updateImagesCount();
+        } else if sender is VideoPopoverVC {
+            updateVideoCount();
+        } else if sender is AudioRecorderVC {
+            updateAudioCount();
+        }
+    }
 ///
 /// Hide the keyboard on done pressed
 ///
-	func textView(textView: UITextView, shouldChangeTextInRange range: NSRange,
-		replacementText text: String) -> Bool {
-			if text == "\n" {
-				textView.resignFirstResponder();
-				return false;
-			}
-			return true;
-	}
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange,
+        replacementText text: String) -> Bool {
+            if text == "\n" {
+                textView.resignFirstResponder();
+                return false;
+            }
+            return true;
+    }
 
-	func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
 
-		if textView.text == enterText {
-			textView.text = "";
-			textView.textColor = UIColor.blackColor();
-		}
-		return true;
-	}
-	func textViewDidEndEditing(textView: UITextView) {
-		if textView.text.characters.count == 0 {
-			textView.text = enterText;
-			textView.textColor = UIColor.lightGrayColor();
-		}
-	}
+        if textView.text == enterText {
+            textView.text = "";
+            textView.textColor = UIColor.blackColor();
+        }
+        return true;
+    }
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text.characters.count == 0 {
+            textView.text = enterText;
+            textView.textColor = UIColor.lightGrayColor();
+        }
+    }
 
-	func updateImagesCount() {
-		let imagesCount = DataManagerInstance().getImagesCount();
-		if imagesCount > 0 {
-			imagesCountLabel.text = String(imagesCount);
-			imagesCountLabel.hidden = false;
-		} else {
-			imagesCountLabel.hidden = true;
-		}
-	}
+    func updateImagesCount() {
+        let imagesCount = DataManagerInstance().getImagesCount();
+        if imagesCount > 0 {
+            imagesCountLabel.text = String(imagesCount);
+            imagesCountLabel.hidden = false;
+        } else {
+            imagesCountLabel.hidden = true;
+        }
+    }
 
-	func updateVideoCount() {
-		if (DataManagerInstance().getTempVideo() != nil) {
-			videoCountLabel.text = "1";
-			videoCountLabel.hidden = false;
-		} else {
-			videoCountLabel.text = "0";
-			videoCountLabel.hidden = true ;
-		}
-	}
+    func updateVideoCount() {
+        if (DataManagerInstance().getTempVideo() != nil) {
+            videoCountLabel.text = "1";
+            videoCountLabel.hidden = false;
+        } else {
+            videoCountLabel.text = "0";
+            videoCountLabel.hidden = true ;
+        }
+    }
 
-	func updateAudioCount() {
-		if (DataManagerInstance().getTempAudio().exists) {
-			audioCountLabel.text = "1";
-			audioCountLabel.hidden = false;
-		} else {
-			audioCountLabel.text = "0";
-			audioCountLabel.hidden = true ;
-		}
-	}
+    func updateAudioCount() {
+        if (DataManagerInstance().getTempAudio().exists) {
+            audioCountLabel.text = "1";
+            audioCountLabel.hidden = false;
+        } else {
+            audioCountLabel.text = "0";
+            audioCountLabel.hidden = true ;
+        }
+    }
 
-	func adaptivePresentationStyleForPresentationController(controller: UIPresentationController)
-		-> UIModalPresentationStyle {
-			return .None;
-	}
-	@IBAction func onAddEntryPressed(sender: AnyObject) {
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController)
+        -> UIModalPresentationStyle {
+            return .None;
+    }
+    @IBAction func onAddEntryPressed(sender: AnyObject) {
 
-		let date = dateString(NSDate());
+        let date = dateString(NSDate());
 
-		// let newDir = DataManagerInstance().moveTempImages(toDir: date);
-		DataManagerInstance().addNewEntry(
-			Entry(entry_id: -1,
-				skill: skill,
-				description: descriptionTextArea.text ?? "",
-				date_time: date,
-				latitude: 1.0,
-				longitude: 1.0,
-				photos: nil,
-				audio: nil,
-				video: nil))
+        // let newDir = DataManagerInstance().moveTempImages(toDir: date);
+        DataManagerInstance().addNewEntry(
+            Entry(entry_id: -1,
+                skill: skill,
+                description: descriptionTextArea.text ?? "",
+                date_time: date,
+                latitude: 1.0,
+                longitude: 1.0,
+                photos: nil,
+                audio: nil,
+                video: nil))
 
-		// self.navigationController?.popToRootViewControllerAnimated(true);
-		self.navigationController?.popViewControllerAnimated(true);
-		//self.tabBarController?.transitionFromViewController(<#T##fromViewController: UIViewController##UIViewController#>, toViewController: <#T##UIViewController#>, duration: <#T##NSTimeInterval#>, options: <#T##UIViewAnimationOptions#>, animations: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>) = 2;
-	}
+        // self.navigationController?.popToRootViewControllerAnimated(true);
+        self.navigationController?.popViewControllerAnimated(true);
+        self.tabBarController?.selectedIndex = 2;
+        // self.tabBarController?.transitionFromViewController(<#T##fromViewController: UIViewController##UIViewController#>, toViewController: <#T##UIViewController#>, duration: <#T##NSTimeInterval#>, options: <#T##UIViewAnimationOptions#>, animations: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>) = 2;
+    }
 }
 
 protocol MediaCountDelegate {
-	func onCountChange(sender: UIViewController);
+    func onCountChange(sender: UIViewController);
 }
 
 protocol MediaContainer: class {
-	var mediaCountDelegate: MediaCountDelegate? { get set }
+    var mediaCountDelegate: MediaCountDelegate? { get set }
 }
