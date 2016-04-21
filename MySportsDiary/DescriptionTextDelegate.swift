7270602147777089f9da9706b09808c19c11e764
Evdzhan Mustafa <evdzhanmustafa@lp3enm3.stunet.aber.ac.uk>
2016-04-21 21:50:52 +0100

    -Fixed the max limit of the uitextfield delegate to 255.

--- MySportsDiary/DescriptionTextDelegate.swift
+++ MySportsDiary/DescriptionTextDelegate.swift
@@ -9,30 +9,37 @@
 import UIKit
 
 class DescriptionTextDelegate: NSObject, UITextViewDelegate {
-    ///
-    /// Hide the keyboard on done pressed
-    ///
-    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
-            textView.resignFirstResponder();
-            return false;
-        }
+	///
+	/// Hide the keyboard on done pressed
+	///
+	func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+		if text == "\n" {
+			textView.resignFirstResponder();
+			return false;
+		}
+		return textView.text.characters.count
+			+ (text.characters.count - range.length) <= 255;
+	}
 
-        return textView.text.characters.count + (text.characters.count - range.length) <= 20
-    }
-
-    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
-
-        if textView.text == enterText {
-            textView.text = "";
-            textView.textColor = UIColor.blackColor();
-        }
-        return true;
-    }
-    func textViewDidEndEditing(textView: UITextView) {
-        if textView.text.characters.count == 0 {
-            textView.text = enterText;
-            textView.textColor = UIColor.lightGrayColor();
-        }
-    }
+	///
+	/// If the text is equal to the "enterText" we delete the contents,
+	/// and change the input color to black.
+	///
+	func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+		if textView.text == enterText {
+			textView.text = "";
+			textView.textColor = UIColor.blackColor();
+		}
+		return true;
+	}
+	///
+	/// If the resulting string is empty, we put the "enterText" string back,
+	/// and change the background color to lightgray.
+	///
+	func textViewDidEndEditing(textView: UITextView) {
+		if textView.text.characters.count == 0 {
+			textView.text = enterText;
+			textView.textColor = UIColor.lightGrayColor();
+		}
+	}
 }
